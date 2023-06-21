b4f454ed16796e675a3f1ecc91776013b92de34e
Nguyễn Đình Dương <duongnd.agiletech@gmail.com>
2023-06-21 04:11:18 +0000

    Update ci script

--- discordhook.sh
+++ discordhook.sh
@@ -112,6 +112,8 @@ fi
 for ARG in "$@"; do
   echo -e "[Webhook]: Sending webhook to Discord...\\n";
 
+  echo $ARG
+
   (curl --fail --progress-bar -A "GitLabCI-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "$WEBHOOK_DATA" "$ARG" \
   && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
 done
