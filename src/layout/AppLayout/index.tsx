e04baa1b0cb22ccd0fd05ddf2d661c355c0f53a3
hunghg255 <giahunghust@gmail.com>
2023-06-22 20:51:59 +0700

    test

--- src/layout/AppLayout/index.tsx
+++ src/layout/AppLayout/index.tsx
@@ -1,4 +1,10 @@
+import { useMount } from 'ahooks';
+
 const AppLayout = ({ children, InterFont }: any) => {
+  useMount(() => {
+    console.log('App Running');
+  });
+
   return (
     <>
       <main className={InterFont.variable}>{children}</main>
