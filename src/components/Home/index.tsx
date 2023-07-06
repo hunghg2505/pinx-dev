b215724739170ff35867de86e44a530c8dc3912a
namnv25 <namnv.agiletech@gmail.com>
2023-07-06 17:23:26 +0700

    update code

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -47,7 +47,6 @@ const Home = () => {
   socket.on('connect', function () {
     requestJoinIndex();
   });
-
   const [selectTab, setSelectTab] = React.useState<string>('2');
   const refScroll = React.useRef(null);
   // const { t } = useTranslation('home');
