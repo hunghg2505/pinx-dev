acc88e10bab4e8fb9f1e27271df6631b2e0bf202
namnv25 <namnv.agiletech@gmail.com>
2023-07-06 09:39:47 +0700

    update comment

--- src/components/Post/PostDetail/index.tsx
+++ src/components/Post/PostDetail/index.tsx
@@ -102,10 +102,9 @@ const PostDetail = () => {
       );
     }
   };
-  if (!postDetail) {
-    return <></>;
-  }
-
+  // if (!postDetail) {
+  //   return <></>;
+  // }
   return (
     <>
       <div className='flex flex-row items-start' ref={refContainer}>
