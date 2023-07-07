03d610b5e691acdb38e058a3af7c40d104f1a1c8
namnv25 <namnv.agiletech@gmail.com>
2023-07-07 11:35:40 +0700

    update ui

--- src/components/Explore/index.tsx
+++ src/components/Explore/index.tsx
@@ -2,10 +2,13 @@ import Form from 'rc-field-form';
 import Slider from 'react-slick';
 
 import ThemeExploreItem from '@components/Themes/ThemeExploreItem';
+import { ExploreButton } from '@components/UI/Button';
 import FormItem from '@components/UI/FormItem';
 import Input from '@components/UI/Input';
 import Text from '@components/UI/Text';
 import { IconSearchWhite } from '@layout/components/MainHeader';
+
+import KeywordSearch from './KeywordSearch';
 
 const settings = {
   dots: false,
@@ -38,14 +41,32 @@ const Explore = () => {
       <Text type='body-20-medium' color='neutral-1' className='mb-[16px] mt-[36px]'>
         Themes
       </Text>
-      <div className='overflow-hidden'>
+      <div className='mb-[16px] overflow-hidden'>
         <Slider {...settings} variableWidth>
           <ThemeExploreItem />
           <ThemeExploreItem />
           <ThemeExploreItem />
         </Slider>
       </div>
-      <div>{/* <Text></Text> */}</div>
+      <ExploreButton>
+        <Text type='body-14-bold' color='primary-2'>
+          Explore themes
+        </Text>
+      </ExploreButton>
+      <div>
+        <Text type='body-20-medium' color='neutral-1' className='mb-[16px] mt-[36px]'>
+          Top keyword search
+        </Text>
+        <div className='mb-[16px] flex flex-col gap-y-[12px]'>
+          <KeywordSearch percen={80} />
+          <KeywordSearch percen={100} />
+        </div>
+        <ExploreButton>
+          <Text type='body-14-bold' color='primary-2'>
+            Explore top search
+          </Text>
+        </ExploreButton>
+      </div>
     </div>
   );
 };
