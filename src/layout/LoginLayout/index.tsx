b032057f49d0208fd50f63e3f2d8ac1caec87b69
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-07-07 17:02:07 +0700

    update style auth layout

--- src/layout/LoginLayout/index.tsx
+++ src/layout/LoginLayout/index.tsx
@@ -8,8 +8,8 @@ const LoginLayout = ({ children }: any) => {
   return (
     <>
       <LoginHeader />
-      <div className='flex w-full justify-center laptop:min-h-[98vh]'>
-        <div className='h-[100vh] w-[40%] flex-col items-center bg-[#6DB6E1] mobile:hidden laptop:flex laptop:min-h-[750px]'>
+      <div className='flex w-full justify-center laptop:min-h-screen'>
+        <div className='w-[40%] flex-col items-center bg-[#6DB6E1] mobile:hidden laptop:flex laptop:min-h-[750px]'>
           <img
             src='/static/images/pinex_logo.png'
             alt=''
@@ -30,7 +30,7 @@ const LoginLayout = ({ children }: any) => {
               width='0'
               height='0'
               sizes='50vw'
-              className='!mt-[15vh] !mb-14 h-[72px] w-[72px] mobile:hidden laptop:block'
+              className='!mb-14 !mt-[15vh] h-[72px] w-[72px] mobile:hidden laptop:block'
             />
             <div className='md:h-screen lg:py-0 mx-auto flex min-w-[98vw] flex-col items-center justify-center px-6 laptop:min-w-min laptop:px-0'>
               <div className='sm:max-w-md md:mt-0 xl:p-0 w-full rounded-lg bg-white'>
