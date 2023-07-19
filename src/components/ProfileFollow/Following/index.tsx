456a989bea14444ed1ccc7c6737ab6a91ace9ffa
andynguyenqtech <dat.bt@agiletechsoftware.com>
2023-07-19 13:59:06 +0700

    update api

--- src/components/ProfileFollow/Following/index.tsx
+++ src/components/ProfileFollow/Following/index.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
 import UserFolow from '@components/common/UserFolow';
 
-// import { useCustomerFollowing } from '../service';
+import NotFound from '../NotFound';
+import { useCustomerFollowing } from '../service';
 
 const Following = () => {
-  // const router = useRouter();
-  // const { data } = useCustomerFollowing(String(router?.query?.search), String(router?.query?.id));
+  const router = useRouter();
+  const { data } = useCustomerFollowing(String(router?.query?.search), String(router?.query?.id));
 
-  // console.log(data);
   return (
     <>
-      <UserFolow
-        avatar='https://i.pinimg.com/236x/27/2f/65/272f654a47b56b7159d7b38d474e93e9.jpg'
-        displayName='Lisa Simpson'
-        isFollow={true}
-        id={556}
-      />
+      {!!data?.data?.length && (
+        <>
+          {!!data?.data?.length && (
+            <>
+              {data?.data?.map((item: any) => (
+                <UserFolow {...item} key={item.id} />
+              ))}
+            </>
+          )}
+        </>
+      )}
+      {!data?.data?.length && <NotFound />}
     </>
   );
 };
