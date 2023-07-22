d18d7599a504cc769fa4410724f16fb7adbf5036
andynguyenqtech <93109662+DatReactJS@users.noreply.github.com>
2023-07-22 10:59:11 +0700

    update css

--- src/components/MyProfile/TabsContent/Following/index.tsx
+++ src/components/MyProfile/TabsContent/Following/index.tsx
@@ -5,20 +5,28 @@ import { useRouter } from 'next/router';
 import Search from '@components/common/Search';
 import UserFolowDesktop from '@components/common/UserFolowDesktop';
 import { useCustomerFollowing } from '@components/MyProfileFollow/service';
+import useLoadMore from '@utils/useLoadmore';
 
 import NotFound from './NotFound';
 
 const Following = () => {
   const router = useRouter();
-  const { data } = useCustomerFollowing(String(router?.query?.search), String(router?.query?.id));
-
+  const { data, run } = useCustomerFollowing(
+    String(router?.query?.search),
+    String(router?.query?.id),
+  );
+  const { lastElementRef } = useLoadMore(run);
   return (
     <>
       <Search />
       <div className='grid grid-cols-4 gap-[14px]'>
-        {data?.data?.map((item: any) => (
-          <UserFolowDesktop {...item} key={item.id} />
-        ))}
+        {data?.data?.map((item: any, index: number) => {
+          return index + 1 === data?.data.length ? (
+            <UserFolowDesktop {...item} key={item.id} ref={lastElementRef} />
+          ) : (
+            <UserFolowDesktop {...item} key={item.id} />
+          );
+        })}
       </div>
       {!data?.data?.length && <NotFound />}
     </>
