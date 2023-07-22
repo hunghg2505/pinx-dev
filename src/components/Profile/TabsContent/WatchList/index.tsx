a77e9e195f3ee016627156d11143be8e9789da66
andynguyenqtech <93109662+DatReactJS@users.noreply.github.com>
2023-07-22 17:36:14 +0700

    asset watch lsit

--- src/components/Profile/TabsContent/WatchList/index.tsx
+++ src/components/Profile/TabsContent/WatchList/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { useRouter } from 'next/router';
+
+import PopupAccessLimit from '@components/UI/Popup/PopupAccessLimit';
+import { useAuth } from '@store/auth/useAuth';
 
 import ComponentWatchList from './ComponentWatchList';
 
 const WatchList = () => {
+  const { isLogin } = useAuth();
+  const [visible, setVisible] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    if (!isLogin && router?.query?.tab === 'watchlist') {
+      setVisible(true);
+    }
+  }, [router.query]);
   return (
     <>
-      <ComponentWatchList />
+      {isLogin && <ComponentWatchList isEdit={false} />}
+      {!isLogin && (
+        <PopupAccessLimit
+          visible={visible}
+          onClose={() => {
+            setVisible(false);
+          }}
+        />
+      )}
     </>
   );
 };
