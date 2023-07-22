de046c46ec786eb42fec377c9e6dff19cb97551a
andynguyenqtech <93109662+DatReactJS@users.noreply.github.com>
2023-07-22 14:46:26 +0700

    update

--- src/components/ProfileFollow/service.ts
+++ src/components/ProfileFollow/service.ts
@@ -3,32 +3,32 @@ import { useRequest } from 'ahooks';
 import { API_PATH } from '@api/constant';
 import { privateRequest, requestPist } from '@api/request';
 
-export const useOtherCustomerFollower = (idCustomer?: string) => {
+export const useOtherCustomerFollower = (page = 1, idCustomer?: string, config?: any) => {
   return useRequest(
     async () => {
       return privateRequest(requestPist.get, API_PATH.GET_CUSTOMER_FOLLOWER, {
         params: {
           idCustomer,
+          page,
+          pageSize: 16,
         },
       });
     },
-    {
-      refreshDeps: [idCustomer],
-    },
+    { ...config },
   );
 };
 
-export const useOtherCustomerFollowing = (idCustomer?: string) => {
+export const useOtherCustomerFollowing = (page = 1, idCustomer?: string, config?: any) => {
   return useRequest(
     async () => {
       return privateRequest(requestPist.get, API_PATH.GET_CUSTOMER_FOLLOWING, {
         params: {
           idCustomer,
+          page,
+          pageSize: 16,
         },
       });
     },
-    {
-      refreshDeps: [idCustomer],
-    },
+    { ...config },
   );
 };
