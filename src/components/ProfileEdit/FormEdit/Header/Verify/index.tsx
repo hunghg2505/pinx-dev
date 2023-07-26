576041bec8238b3848467d29678010fd9614edfa
andynguyenqtech <93109662+DatReactJS@users.noreply.github.com>
2023-07-26 11:48:11 +0700

    delete check icon

--- src/components/ProfileEdit/FormEdit/Header/Verify/index.tsx
+++ src/components/ProfileEdit/FormEdit/Header/Verify/index.tsx
@@ -12,21 +12,6 @@ const Verify = () => {
     <div className='mb-[20px]'>
       {profileUser?.acntStat === 'ACTIVE' && (
         <span className='text[12px] line-[16px] font[500] mx-auto flex items-center justify-center gap-[4px] text-green'>
-          <svg
-            width='16'
-            height='16'
-            viewBox='0 0 16 16'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M13.3327 4L5.99935 11.3333L2.66602 8'
-              stroke='#128F63'
-              strokeWidth='1.7'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-          </svg>
           {t('verified')}
         </span>
       )}
