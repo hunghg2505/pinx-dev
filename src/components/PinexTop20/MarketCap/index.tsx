f825ecb795a84a3c26ba6836cf46343d12f063d3
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-07-27 15:48:56 +0700

    update locale

--- src/components/PinexTop20/MarketCap/index.tsx
+++ src/components/PinexTop20/MarketCap/index.tsx
@@ -1,17 +1,21 @@
+import { useTranslation } from 'next-i18next';
+
 import Text from '@components/UI/Text';
 
 import PinexTop from '../PinexTop';
 import { IProfit, useGetTopMarketCap } from '../service';
 
 const MarketCap = () => {
+  const { t } = useTranslation('explore');
+
   const { marketCap } = useGetTopMarketCap();
   return (
     <div className=''>
       <Text type='body-14-regular' color='cbblack'>
-        Top 20 companies having the largest capitalization
+        {t('top_20_tab.market_cap_desc')}
       </Text>
       <Text type='body-12-regular' color='cbblack' className='text-right italic'>
-        Unit: Trillion VND
+        {t('top_20_tab.market_cap_unit')}
       </Text>
       <div className='mt-[16px] flex flex-col gap-y-[16px]'>
         {marketCap?.map((marketCap: IProfit, index: number) => {
