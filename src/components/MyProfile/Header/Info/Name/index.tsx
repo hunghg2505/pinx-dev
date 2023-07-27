8de220f5970504cfb23c94e7420d847221c718df
andynguyenqtech <93109662+DatReactJS@users.noreply.github.com>
2023-07-27 17:26:10 +0700

    long text

--- src/components/MyProfile/Header/Info/Name/index.tsx
+++ src/components/MyProfile/Header/Info/Name/index.tsx
@@ -10,7 +10,7 @@ const Name = () => {
   return (
     <>
       <div className='mb-[4px] flex gap-[8px] tablet:absolute  tablet:bottom-[calc(100%+32px)] '>
-        <h3 className='text-[20px] font-[600] truncate w-[calc(100%-188px)]'>{profileUser?.displayName}</h3>
+        <h3 className='text-[20px] font-[600] truncate w-[max(calc(100%-188px),370px)]'>{profileUser?.displayName}</h3>
         {profileUser?.isFeatureProfile && <IsFeatureProfile />}
         {profileUser?.isKol && <IsKol />}
       </div>
