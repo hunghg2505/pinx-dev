f1be6ff6a54624bb31d76180520aa4491945eaf9
hailm <hai.lemanh.1310@gmail.com>
2023-07-27 17:27:57 +0700

    Update index.tsx

--- src/components/WatchList/index.tsx
+++ src/components/WatchList/index.tsx
@@ -17,6 +17,7 @@ import Text from '@components/UI/Text';
 import Themes from '@components/WatchList/Themes';
 // @ts-ignore
 import YourWatchList from '@components/WatchList/YourWatchList';
+import { useResponsive } from '@hooks/useResponsive';
 
 import ModalAddStock from './ModalAddStock';
 import { useGetInterest, useGetYourWatchList } from './service';
@@ -36,6 +37,7 @@ const WatchList = () => {
   const [watchlistId, setWatchlistId] = React.useState<number>();
   const [dataStock, setDataStock] = React.useState<any>([]);
   const router = useRouter();
+  const { isMobile } = useResponsive();
   const onGoBack = () => {
     router.back();
   };
@@ -80,12 +82,12 @@ const WatchList = () => {
 
   return (
     <div className='flex flex-col gap-y-[32px] rounded-[8px] bg-white px-[10px] py-[20px] desktop:gap-y-[20px] desktop:px-[24px]'>
-      <div className='flex flex-col gap-y-[16px] desktop:gap-y-[20px]'>
-        {!isEdit && (
+      {yourWatchListStock?.length > 0 && (<div className='flex flex-col gap-y-[16px] desktop:gap-y-[20px]'>
+        {!isEdit && isMobile && (
           <img
             src='/static/icons/back_icon.svg'
             alt=''
-            className='w-[28px] cursor-pointer desktop:hidden'
+            className='w-[28px] cursor-pointer'
             onClick={onGoBack}
           />
         )}
@@ -161,7 +163,7 @@ const WatchList = () => {
           itemDelete={itemDelete}
           setItemDelete={setItemDelete}
         />
-      </div>
+      </div>)}
       <Interest
         isEdit={isEdit}
         interestStock={interestStock}
