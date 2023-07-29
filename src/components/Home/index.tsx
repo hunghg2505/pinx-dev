4d89ca093ce3e2c41a5c6d2ff84ba128de10278b
hailm <hai.lemanh.1310@gmail.com>
2023-07-29 16:11:02 +0700

    fix: fig bug ui error Fix Error: Hydration failed because the initial UI does not match what was rendered on the server.

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,8 @@
+import React from 'react';
+
 import dynamic from 'next/dynamic';
+
+import Skeleton from '@components/UI/Skeleton';
 
 const ComposeButton = dynamic(() => import('./ComposeButton'));
 const PopupHomeNoti = dynamic(() => import('@components/Home/PopupHomeNoti/PopupHomeNoti'));
@@ -6,6 +10,20 @@ const FooterSignUp = dynamic(() => import('@components/FooterSignup'));
 const ModalComposeMobile = dynamic(() => import('@components/Compose/ModalComposeMobile'));
 const HomeNewFeed = dynamic(() => import('@components/Home/HomeNewFeed/HomeNewFeed'));
 const Home = ({ pinPostData }: any) => {
+  // Fix Error: Hydration failed because the initial UI does not match what was rendered on the server.
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setMounted(true);
+    },0);
+  }, []);
+
+  // For Next.js 13, return jsx once the component is mounted
+  if (!mounted) {
+    return <Skeleton/>;
+  }
+
   return (
     <>
       <PopupHomeNoti />
