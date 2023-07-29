e10f56d733b56dc21fb877a3995c95a053030166
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-07-29 13:49:40 +0700

    update UI style profile

--- src/components/MyProfile/Header/Info/index.tsx
+++ src/components/MyProfile/Header/Info/index.tsx
@@ -15,21 +15,34 @@ const Info = () => {
   return (
     <div className='relative flex w-full grid-flow-col items-center gap-2 pl-[16px] pr-[16px]  pt-[20px] tablet:pl-[132px] tablet:pr-0 xdesktop:mt-[0] xdesktop:pl-[168px] '>
       <div className='flex-auto'>
-        <Name
-          displayName={profileUser.displayName}
-          isKol={profileUser.isKol}
-          isFeatureProfile={profileUser.isFeatureProfile}
-        />
-        <Position position={profileUser?.position} />
-        <div className='mb-[5px] flex gap-[8px] xdesktop:gap-[8px]'>
-          <Follower totalFollower={profileUser?.totalFollower} />
-          <span className='text-[#808A9D]'>&bull;</span>
-          <Following totalFollowing={profileUser?.totalFollowing} />
+        <div className='mb-[8px] flex items-center justify-between tablet:mb-[4px]'>
+          <Name
+            displayName={profileUser.displayName}
+            isKol={profileUser.isKol}
+            isFeatureProfile={profileUser.isFeatureProfile}
+          />
+          <EditDeskTop />
         </div>
-      </div>
-      <div className='flex-none flex-col items-start justify-start'>
-        <EditDeskTop />
-        <Joined year={new Date(profileUser?.createdAt)?.getFullYear()} />
+
+        <Position position={profileUser?.position} />
+
+        <div className='mb-[5px] flex items-center justify-between'>
+          <div>
+            <div className='flex gap-[8px] xdesktop:gap-[8px]'>
+              <Follower totalFollower={profileUser?.totalFollower} />
+              <span className='text-[#808A9D]'>&bull;</span>
+              <Following totalFollowing={profileUser?.totalFollowing} />
+            </div>
+
+            <div className='mt-[8px] flex justify-start tablet:hidden'>
+              <Joined year={new Date(profileUser?.createdAt)?.getFullYear()} />
+            </div>
+          </div>
+
+          <div className='hidden tablet:block'>
+            <Joined year={new Date(profileUser?.createdAt)?.getFullYear()} />
+          </div>
+        </div>
       </div>
     </div>
   );
