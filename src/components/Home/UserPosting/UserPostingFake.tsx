b199e9f7332b622c6de79297126a70d8b1c5d114
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-07-31 14:34:52 +0700

    update UI user posting

--- src/components/Home/UserPosting/UserPostingFake.tsx
+++ src/components/Home/UserPosting/UserPostingFake.tsx
@@ -6,26 +6,23 @@ const UserPostingFake = () => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <div className='box-shadow card-style rounded-[8px] bg-[#FFFFFF] p-[20px] mobile:hidden tablet:mb-[20px] tablet:block desktop:h-[174px]'>
-        <div className='flex items-center'>
-          <img
-            src={'/static/images/img-blur.png'}
-            alt=''
-            width={0}
-            height={0}
-            sizes='100vw'
-            className='mr-[10px] h-[56px] w-[56px] cursor-pointer rounded-full object-cover'
-          />
-        </div>
-        <div className='mt-[5px] pl-[61px]'>
-          <textarea
-            placeholder={t('what_is_in_your_mind')}
-            className='w-full rounded-[5px] bg-[#EFF2F5] pl-[10px] pt-[10px] focus:outline-none desktop:h-[70px]'
-          />
-        </div>
+    <div className='box-shadow card-style rounded-[12px] bg-[#fff] mobile:hidden tablet:mb-[20px] tablet:block'>
+      <div className='flex items-center'>
+        <img
+          src='/static/images/img-blur.png'
+          alt=''
+          width={0}
+          height={0}
+          sizes='100vw'
+          className='mr-[10px] h-[56px] w-[56px] cursor-pointer rounded-full object-cover'
+        />
+        <input
+          type='text'
+          className='h-[36px] w-full flex-1 rounded-[5px] bg-[#fff] p-[10px] focus:outline-none'
+          placeholder={t('what_is_on_your_mind')}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
