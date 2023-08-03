4bf3723293eec706ec97ba89d32aa2b3e598ea55
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-08-03 20:45:40 +0700

    fix slider stock product

---
+++ src/components/Stock/StockDetail/ProductItem/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import classNames from 'classnames';
+
+import { IProduct } from '@components/Stock/type';
+import Text from '@components/UI/Text';
+import { PRODUCT_COMPANY_IMAGE } from 'src/constant';
+
+interface IProductItemProps {
+  data: IProduct;
+  className?: string;
+}
+
+const ProductItem = ({ data, className }: IProductItemProps) => {
+  return (
+    <div className={classNames('mr-[28px] !w-[112px]', className)}>
+      <img
+        src={PRODUCT_COMPANY_IMAGE(data.imageUrl)}
+        alt={data.name}
+        className='h-[112px] w-full rounded-[4px] object-cover'
+      />
+
+      <Text className='mt-[12px] text-center' type='body-12-regular'>
+        {data.name}
+      </Text>
+    </div>
+  );
+};
+
+export default ProductItem;
