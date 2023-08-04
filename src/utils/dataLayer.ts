f1b39ec0d1e76d8dd91c30856eff16c7dee2913d
namnv25 <namnv.agiletech@gmail.com>
2023-08-04 11:54:46 +0700

    update code

--- src/utils/dataLayer.ts
+++ src/utils/dataLayer.ts
@@ -1,18 +1,14 @@
 declare const window: any;
 
 export const Logout = (time: Date) => {
-  console.log('🚀 ~ file: dataLayer.ts:4 ~ Logout ~ time:', time);
   try {
-    console.log('window.dataLayer.push');
     window.dataLayer.push({
       event: 'Logout',
       'Login Status': 'Not Login',
       'Last Seen Time': time,
       'Last Seen App': 'PineX Website',
     });
-  } catch {
-    console.log('window.dataLayer.push-err');
-  }
+  } catch {}
 };
 
 export const NavigateSection = (navigate: string) => {
