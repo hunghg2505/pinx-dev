a62ccf66cb4393a0783477be4ea45fac3971a42c
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-08-07 10:09:08 +0700

    fix UI stock

--- src/components/Stock/StockDetail/FinancialQuartersTab/index.tsx
+++ src/components/Stock/StockDetail/FinancialQuartersTab/index.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'next-i18next';
 import { manualTranslateFinancialIndicator } from '@components/Stock/const';
 import { useFinancialIndicator } from '@components/Stock/service';
 import { FinancialAnnualKey, FinancialType } from '@components/Stock/type';
+import Loading from '@components/UI/Loading';
 import Text from '@components/UI/Text';
 
 interface IFinancialQuartersTabProps {
@@ -15,6 +16,7 @@ interface IFinancialQuartersTabProps {
 const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
   const { i18n, t } = useTranslation(['stock']);
   const [page, setPage] = useState(1);
+  const [isNextPage, setIsNextPage] = useState(false);
   const { financialIndicator, onGetFinancialIndicator, loading } = useFinancialIndicator({
     params: {
       stockCode,
@@ -26,9 +28,13 @@ const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
     !loading && onGetFinancialIndicator(page);
   }, [page]);
 
-  const handleBack = () => setPage((prev) => prev + 1);
+  const handleBack = () => {
+    setIsNextPage(false);
+    setPage((prev) => prev + 1);
+  };
   const handleNext = () => {
     if (page > 1) {
+      setIsNextPage(true);
       setPage((prev) => prev - 1);
     }
   };
@@ -37,17 +43,23 @@ const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
     <table className=' w-full table-fixed border-collapse overflow-hidden rounded-[12px] border-hidden shadow-[0_0_0_1px_var(--neutral-7)]'>
       <thead>
         <tr className='h-[78px]'>
-          <th className='align-middle'>
-            {financialIndicator?.data && financialIndicator?.data?.hasBack && (
-              <img
-                src='/static/icons/back_icon.svg'
-                alt='Previous icon'
-                className='ml-[12px] h-[24px] w-[24px] cursor-pointer object-contain'
-                onClick={handleBack}
-              />
-            )}
+          <th className='align-middle' colSpan={1}>
+            {financialIndicator?.data &&
+              financialIndicator?.data?.hasBack &&
+              (loading && !isNextPage ? (
+                <div className='ml-[12px]'>
+                  <Loading />
+                </div>
+              ) : (
+                <img
+                  src='/static/icons/back_icon.svg'
+                  alt='Previous icon'
+                  className='ml-[12px] h-[24px] w-[24px] cursor-pointer object-contain'
+                  onClick={handleBack}
+                />
+              ))}
           </th>
-          <th className='align-middle'>
+          <th className='align-middle' colSpan={4}>
             <Text type='body-14-bold' className='mb-[12px] text-[#0D0D0D]'>
               {i18n.language === 'vi'
                 ? financialIndicator?.data?.head.termName
@@ -59,22 +71,30 @@ const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
               {dayjs(financialIndicator?.data?.head.periodEnd).format('MM/YYYY')}
             </Text>
           </th>
-          <th className='text-right align-middle'>
-            {page > 1 && (
-              <img
-                src='/static/icons/back_icon.svg'
-                alt='Next icon'
-                className='ml-auto mr-[12px] h-[24px] w-[24px] rotate-180 cursor-pointer object-contain'
-                onClick={handleNext}
-              />
-            )}
+          <th className='text-right align-middle' colSpan={1}>
+            {page > 1 &&
+              (loading && isNextPage ? (
+                <div className='mr-[12px]'>
+                  <Loading className='inline-block' />
+                </div>
+              ) : (
+                <img
+                  src='/static/icons/back_icon.svg'
+                  alt='Next icon'
+                  className='ml-auto mr-[12px] h-[24px] w-[24px] rotate-180 cursor-pointer object-contain'
+                  onClick={handleNext}
+                />
+              ))}
           </th>
         </tr>
       </thead>
 
       <tbody>
         <tr>
-          <td className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'>
+          <td
+            colSpan={2}
+            className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'
+          >
             <Text type='body-14-medium' className='text-[#999999]'>
               {t(manualTranslateFinancialIndicator(FinancialAnnualKey.EPS))}
             </Text>
@@ -84,7 +104,10 @@ const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
             </Text>
           </td>
 
-          <td className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'>
+          <td
+            colSpan={2}
+            className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'
+          >
             <Text type='body-14-medium' className='uppercase text-[#999999]'>
               {t(manualTranslateFinancialIndicator(FinancialAnnualKey.BVPS))}
             </Text>
@@ -94,7 +117,10 @@ const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
             </Text>
           </td>
 
-          <td className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'>
+          <td
+            colSpan={2}
+            className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'
+          >
             <Text type='body-14-medium' className='uppercase text-[#999999]'>
               {t(manualTranslateFinancialIndicator(FinancialAnnualKey.PE))}
             </Text>
@@ -105,7 +131,10 @@ const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
           </td>
         </tr>
         <tr>
-          <td className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'>
+          <td
+            colSpan={2}
+            className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'
+          >
             <Text type='body-14-medium' className='uppercase text-[#999999]'>
               {t(manualTranslateFinancialIndicator(FinancialAnnualKey.ROS))}
             </Text>
@@ -115,7 +144,10 @@ const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
             </Text>
           </td>
 
-          <td className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'>
+          <td
+            colSpan={2}
+            className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'
+          >
             <Text type='body-14-medium' className='uppercase text-[#999999]'>
               {t(manualTranslateFinancialIndicator(FinancialAnnualKey.ROEA))}
             </Text>
@@ -125,7 +157,10 @@ const FinancialQuartersTab = ({ stockCode }: IFinancialQuartersTabProps) => {
             </Text>
           </td>
 
-          <td className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'>
+          <td
+            colSpan={2}
+            className='border border-solid border-[var(--neutral-7)] bg-[#F7F6F8] py-[12px] text-center'
+          >
             <Text type='body-14-medium' className='uppercase text-[#999999]'>
               {t(manualTranslateFinancialIndicator(FinancialAnnualKey.ROAA))}
             </Text>
