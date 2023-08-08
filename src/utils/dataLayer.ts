a120aeb69bbfd6c2be407b7cd319efaeaf02a898
namnv25 <namnv.agiletech@gmail.com>
2023-08-08 14:54:08 +0700

    update code

--- src/utils/dataLayer.ts
+++ src/utils/dataLayer.ts
@@ -29,7 +29,7 @@ export const GetMoreInfo = (screenName: string, infoGroup: string, infoDetail: s
     });
   } catch {}
 };
-export const Register = (startRegistration: Date, CTALocaiton: string, CTAType: string) => {
+export const RegisterTracking = (startRegistration: Date, CTALocaiton: string, CTAType: string) => {
   try {
     window.dataLayer.push({
       event: 'Register',
