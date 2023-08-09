36b7d91fce47e03de51ab6dbab84e9df96246b2c
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-08-09 09:18:18 +0700

    fix style stock detail

--- src/components/Stock/StockDetail/index.tsx
+++ src/components/Stock/StockDetail/index.tsx
@@ -73,7 +73,7 @@ const MAX_LINE = 4;
 const LINE_HEIGHT = 21;
 const MAX_HEIGHT = MAX_LINE * LINE_HEIGHT;
 const STOCK_EVENT_ITEM_LIMIT = 4;
-const WATCHING_INVESTING_ITEM_LIMIT = 5;
+const WATCHING_INVESTING_ITEM_LIMIT = 4;
 const HIGHLIGH_ROW_LIMIT = 3;
 const ALSO_ITEM_LIMIT = 2;
 const NEWS_ITEM_LIMIT = 3;
@@ -444,7 +444,7 @@ const StockDetail = () => {
         </div>
 
         <div className='mt-[12px] flex items-center justify-between'>
-          <div className='flex flex-col gap-y-[8px] tablet:flex-row tablet:gap-x-[12px]'>
+          <div className='flex flex-1 flex-col gap-y-[8px] tablet:flex-row tablet:gap-x-[12px]'>
             <div className='flex h-[44px] w-[44px] items-center rounded-[12px] border border-solid border-[#EEF5F9] bg-white px-[5px] shadow-[0_1px_2px_0_rgba(88,102,126,0.12),0px_4px_24px_0px_rgba(88,102,126,0.08)]'>
               <img
                 src={imageStock(stockCode)}
@@ -885,7 +885,7 @@ const StockDetail = () => {
               {t('community_description')}
             </Text>
 
-            <div className='mt-[16px] flex items-center justify-between tablet:justify-start'>
+            <div className='mb-[8px] mt-[16px] flex items-center justify-between tablet:justify-start'>
               <Link href={ROUTE_PATH.STOCK_SUBSCRIBER(stockCode)} className='flex gap-x-[10px]'>
                 {stockDetails?.data.watchingInvestingList
                   .slice(0, WATCHING_INVESTING_ITEM_LIMIT)
