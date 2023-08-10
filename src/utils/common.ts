2c127a3635eedef8ca71257a0f94eeb40e30467a
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-08-10 11:43:09 +0700

    update func kFormatter

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -675,7 +675,7 @@ export const getStockColor = (
 
 export const kFormatter = (num: number) => {
   return Math.abs(num) > 999
-    ? Math.sign(num) * +(Math.abs(num) / 1000).toFixed(1) + 'K'
+    ? formatStringToNumber((Math.sign(num) * +(Math.abs(num) / 1000).toFixed(1)).toString()) + 'K'
     : Math.sign(num) * Math.abs(num);
 };
 
