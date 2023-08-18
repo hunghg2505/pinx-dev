1d8c59e48ed240b42b5b56cef4233ca4eb4585c2
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-08-18 11:31:21 +0700

    create skeleton component

--- src/components/UI/Skeleton/index.tsx
+++ src/components/UI/Skeleton/index.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable unicorn/no-useless-spread */
+import { useMemo } from 'react';
+
 import classNames from 'classnames';
 
 import styles from './index.module.scss';
@@ -29,4 +32,52 @@ const SkeletonLoading = ({
     </div>
   );
 };
+
+interface ISkeleton {
+  width?: number;
+  height?: number;
+  avatar?: boolean;
+  className?: string;
+  wrapClassName?: string;
+  round?: boolean;
+  rows?: number;
+  active?: boolean;
+}
+
+export const Skeleton = (props: ISkeleton) => {
+  const { width, height, avatar, className, round, rows = 1, active = true, wrapClassName } = props;
+
+  const styleObj = useMemo(() => {
+    const style: any = {};
+    if (width) {
+      style.width = width + 'px';
+    }
+
+    if (height) {
+      style.height = height + 'px';
+    }
+
+    if (active) {
+      style.animation = '1.5s shine linear infinite';
+    }
+
+    return style;
+  }, [width, height, active]);
+
+  return (
+    <div className={classNames('flex flex-col gap-y-[8px]', wrapClassName)}>
+      {[...new Array(rows)].map((_, index) => (
+        <div
+          key={index}
+          className={classNames(styles.skeleton, className, {
+            '!h-[36px] !w-[36px] !rounded-full': avatar,
+            '!rounded-full': round,
+          })}
+          style={styleObj}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
 export default SkeletonLoading;
