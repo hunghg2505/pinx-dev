341e53cdc0901afa1e1814bfd8fb3f5d41481404
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-08-28 15:42:39 +0700

    add link to display name user comment

--- src/components/Post/NewsFeed/ItemComment/index.tsx
+++ src/components/Post/NewsFeed/ItemComment/index.tsx
@@ -374,7 +374,14 @@ const ItemComment = (props: IProps) => {
         >
           <div className='relative mb-[8px] flex-1 rounded-[12px] bg-[#F3F2F6] pt-[12px]'>
             <div className='flex w-full flex-row items-center justify-between px-[16px]'>
-              <div className='relative flex items-center overflow-hidden mobile:max-w-[150px] laptop:max-w-[300px]'>
+              <div
+                onClick={() =>
+                  isComment
+                    ? router.push(ROUTE_PATH.MY_PROFILE)
+                    : router.push(ROUTE_PATH.PROFILE_DETAIL(data?.customerId))
+                }
+                className='relative flex cursor-pointer items-center overflow-hidden mobile:max-w-[150px] laptop:max-w-[300px]'
+              >
                 <Text type='body-14-semibold' color='neutral-1' className='truncate'>
                   {data?.customerInfo?.displayName}
                 </Text>
