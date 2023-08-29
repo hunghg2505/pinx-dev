4128043755d3c318a2d684503bf9bb62ab3a4d7e
namnv25 <namnv.agiletech@gmail.com>
2023-08-29 11:16:59 +0700

    update code

--- src/components/WatchList/ItemWatchList.tsx
+++ src/components/WatchList/ItemWatchList.tsx
@@ -31,7 +31,7 @@ const ItemWatchList = ({
   const isHigh = data?.lastPrice === data?.ceilPrice;
   const isDecrease = data?.lastPrice < highest_price;
   const isIncrease = data?.lastPrice > lowest_price;
-  const isChange = Number(data?.changePc) === 0 && Number(data?.changePercent) === 0;
+  // const isChange = Number(data?.changePc) === 0 && Number(data?.changePercent) === 0;
   const isNoChange = Number(data?.changePc) === 0 || Number(data?.changePercent) === 0;
   const unit = isDecrease ? '-' : '+';
   const imageCompanyUrl = 'https://static.pinetree.com.vn/upload/images/companies/';
@@ -118,13 +118,13 @@ const ItemWatchList = ({
               'text-[#782AF9]': isHigh,
               'text-[#F1BA09] ': Number(data?.change) === 0 && Number(data?.lastPrice) !== 0,
               'text-[#474D57]': Number(data?.lastPrice) === 0,
-              [style.isIncrease]: isIncrease && !isHigh && isChangeStock && !isChange,
+              [style.isIncrease]: isIncrease && !isHigh && isChangeStock && !isNoChange,
               [style.isDecrease]:
                 isDecrease &&
                 !isFloor &&
                 Number(data?.lastPrice) !== 0 &&
                 isChangeStock &&
-                !isChange,
+                !isNoChange,
             })}
           >
             {Number(data?.lastPrice) === 0 ? '-' : formatStringToNumber(data?.lastPrice, true, 2)}
@@ -138,17 +138,17 @@ const ItemWatchList = ({
               'text-[#782AF9]': isHigh,
               'text-[#F1BA09]  ': Number(data?.change) === 0 && Number(data?.lastPrice) !== 0,
               'text-[#474D57]': Number(data?.lastPrice) === 0,
-              [style.isIncrease]: isIncrease && !isHigh && isChangeStock && !isChange,
+              [style.isIncrease]: isIncrease && !isHigh && isChangeStock && !isNoChange,
               [style.isDecrease]:
                 isDecrease &&
                 !isFloor &&
                 Number(data?.lastPrice) !== 0 &&
                 isChangeStock &&
-                !isChange,
+                !isNoChange,
             })}
           >
             {isNoChange ? '' : unit}
-            {Number(data?.change) === 0 ? '-' : formatStringToNumber(data?.change, true, 2)} /{' '}
+            {isNoChange ? '-' : formatStringToNumber(data?.change, true, 2)} /{' '}
             {isNoChange ? '' : unit}
             {isNoChange
               ? '-'
