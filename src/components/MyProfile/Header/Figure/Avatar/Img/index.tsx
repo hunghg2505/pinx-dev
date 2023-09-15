e74178f91a1484e117316f531fb9f331ae86f8c3
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-09-15 11:24:06 +0700

    fix style avatar profile mobile

--- src/components/MyProfile/Header/Figure/Avatar/Img/index.tsx
+++ src/components/MyProfile/Header/Figure/Avatar/Img/index.tsx
@@ -6,7 +6,7 @@ import { isUrlValid } from '@utils/common';
 
 const Img = ({ avatar, displayName }: { avatar: string; displayName: string }) => {
   return isUrlValid(avatar) ? (
-    <div className='h-[113px] w-[113px] rounded-full bg-white p-[5px] galaxy-max:h-[90px] galaxy-max:w-[90px] tablet:h-[100px] tablet:w-[100px] xdesktop:h-[120px] xdesktop:w-[120px]'>
+    <div className='h-[128px] w-[128px] rounded-full bg-white p-[5px] galaxy-max:h-[90px] galaxy-max:w-[90px] tablet:h-[100px] tablet:w-[100px] xdesktop:h-[120px] xdesktop:w-[120px]'>
       <CustomImage
         width='0'
         height='0'
@@ -17,7 +17,7 @@ const Img = ({ avatar, displayName }: { avatar: string; displayName: string }) =
       />
     </div>
   ) : (
-    <div className='flex h-[113px] w-[113px] items-center justify-center overflow-hidden rounded-full bg-white object-cover p-[5px] galaxy-max:h-[90px] galaxy-max:w-[90px] tablet:h-[100px] tablet:w-[100px] xdesktop:h-[120px] xdesktop:w-[120px]'>
+    <div className='flex h-[128px] w-[128px] items-center justify-center overflow-hidden rounded-full bg-white object-cover p-[5px] galaxy-max:h-[90px] galaxy-max:w-[90px] tablet:h-[100px] tablet:w-[100px] xdesktop:h-[120px] xdesktop:w-[120px]'>
       <AvatarDefault className='!m-0' name={displayName} />
     </div>
   );
