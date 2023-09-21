2ec4a9d8e7a4df16f07c82c497b29b958a902220
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-09-21 15:26:38 +0700

    create post sitemap

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1032,3 +1032,20 @@ export const slugify = (value?: string) => {
   // Convert to lowercase
   return slug.toLowerCase();
 };
+
+export const formatTitlePost = (title: string) => {
+  if (!title) {
+    return '';
+  }
+
+  let titleFormat = title;
+  const userMentionPattern = /@\[(.*?)]\((.*?)\)/g;
+  const stockMentionPattern = /%\[(.*?)]\((.*?)\)/g;
+  const hashtagPattern = /#(\S+)/g;
+
+  titleFormat = titleFormat.replaceAll(userMentionPattern, (_, b) => b);
+  titleFormat = titleFormat.replaceAll(stockMentionPattern, (_, b) => b);
+  titleFormat = titleFormat.replaceAll(hashtagPattern, (_, b) => b);
+
+  return titleFormat;
+};
