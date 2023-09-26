3d7123f3d6dc7c195154727e8f442b8282f84ec7
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2023-09-26 09:11:13 +0700

    update sitemap

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1020,17 +1020,35 @@ export const slugify = (value?: string) => {
     return '';
   }
 
-  // Remove diacritics
-  const withoutDiacritics = value.normalize('NFD').replaceAll(/[\u0300-\u036F]/g, '');
+  // Chuyển hết sang chữ thường
+  let str = value.toLowerCase();
 
-  // Replace spaces and special characters with hyphens
-  const slug = withoutDiacritics
-    .replaceAll(/[^\d\sA-Za-z]/g, '')
-    .trim()
-    .replaceAll(/\s+/g, '-'); // Replace spaces with hyphens
+  // xóa dấu
+  str = str.replaceAll(/([àáâãăạảấầẩẫậắằẳẵặ])/g, 'a');
+  str = str.replaceAll(/([èéêẹẻẽếềểễệ])/g, 'e');
+  str = str.replaceAll(/([ìíĩỉị])/g, 'i');
+  str = str.replaceAll(/([òóôõơọỏốồổỗộớờởỡợ])/g, 'o');
+  str = str.replaceAll(/([ùúũưụủứừửữự])/g, 'u');
+  str = str.replaceAll(/([ýỳỵỷỹ])/g, 'y');
+  str = str.replaceAll(/(đ)/g, 'd');
 
-  // Convert to lowercase
-  return slug.toLowerCase();
+  // Xóa ký tự đặc biệt
+  str = str.replaceAll(/([^\d\sa-z-])/g, '');
+
+  // Xóa khoảng trắng thay bằng ký tự -
+  str = str.replaceAll(/(\s+)/g, '-');
+
+  // Xóa ký tự - liên tiếp
+  str = str.replaceAll(/-+/g, '-');
+
+  // xóa phần dự - ở đầu
+  str = str.replaceAll(/^-+/g, '');
+
+  // xóa phần dư - ở cuối
+  str = str.replaceAll(/-+$/g, '');
+
+  // return
+  return str;
 };
 
 /**
