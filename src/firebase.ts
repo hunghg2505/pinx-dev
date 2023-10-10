0901832bd0f7dfec976df1ad99d7debddbbce5a4
hunghg255 <giahunghust@gmail.com>
2023-10-10 10:09:13 +0700

    chore: update noti

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,12 @@
-/* eslint-disable no-console */
-/* eslint-disable import/no-mutable-exports */
-// Import the functions you need from the SDKs you need
-import { getAnalytics } from 'firebase/analytics';
-import { initializeApp } from 'firebase/app';
-import firebase from 'firebase/compat/app';
+// import { getAnalytics } from 'firebase/analytics';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+// import firebase from 'firebase/compat/app';
 import { getMessaging, getToken, isSupported, onMessage } from 'firebase/messaging';
-import { isIOS } from 'react-device-detect';
+import localforage from 'localforage';
 
 import { ENV } from '@utils/env';
 
 import { allowNotificationTracking } from './mixpanel/mixpanel';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -25,58 +20,116 @@ const firebaseConfig = {
   measurementId: 'G-KK9XPVRSXB',
 };
 
-// Initialize Firebase
-let analytics;
-let firestore;
-let messaging: any;
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-if (firebaseConfig?.projectId && firebase.apps.length === 0) {
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const firebaseCloudMessaging = {
+  tokenInLocalForage: async () => {
+    const token = await localforage.getItem('fcm_token_client');
+    return token;
+  },
+  removeFcmToken: async () => {
+    await localforage.removeItem('fcm_token_client');
+  },
+  onMessage: async (onCallback: any) => {
+    try {
+      const messaging = getMessaging();
+      onMessage(messaging, onCallback);
+    } catch {}
+  },
 
-  if (app.name && typeof window !== 'undefined') {
-    analytics = getAnalytics(app);
-    // eslint-disable-next-line unicorn/prefer-top-level-await
-    isSupported().then((value) => {
-      if (value) {
-        messaging = getMessaging(app);
+  init: async function () {
+    try {
+      let fcmToken = await this.tokenInLocalForage();
+      if (fcmToken !== null) {
+        console.error('fcm_token_client', fcmToken);
+        return fcmToken;
       }
-    });
-    if (!isIOS) {
-      // eslint-disable-next-line unicorn/prefer-top-level-await
-      Notification.requestPermission().then((value) => {
-        if (value && value === 'granted') {
-          allowNotificationTracking('Allow');
-        } else {
-          allowNotificationTracking('Not Allow');
-        }
-      });
-    }
-  }
-} else {
-  firebase.app();
-}
+      // const analytics = getAnalytics(app);
 
-const getMessagingToken = async () => {
-  let currentToken = '';
-  if (!messaging) {
-    return;
-  }
-  try {
-    currentToken = await getToken(messaging, {
-      vapidKey: ENV.FIREBASE_VAPIDKEY,
-    });
-  } catch (error) {
-    console.log('xxx An error occurred while retrieving token.', error);
-  }
-  return currentToken;
+      const isSupport = await isSupported();
+
+      if (!isSupport) {
+        return isSupport;
+      }
+
+      const messaging = getMessaging(app);
+
+      await Notification.requestPermission();
+
+      fcmToken = await getToken(messaging, {
+        vapidKey: ENV.FIREBASE_VAPID_KEY,
+      });
+
+      if (fcmToken) {
+        // Send the token to your server and update the UI if necessary
+        // save the token in your database
+        localforage.setItem('fcm_token_client', fcmToken);
+        console.error('fcm_token_client', fcmToken);
+        allowNotificationTracking('Allow');
+
+        return fcmToken;
+      } else {
+        // Show permission request UI
+        console.error(
+          'NOTIFICACION, No registration token available. Request permission to generate one.',
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      allowNotificationTracking('Not Allow');
+    }
+  },
 };
 
-const onMessageListener = () =>
-  new Promise((resolve) => {
-    onMessage(messaging, (payload) => {
-      resolve(payload);
-    });
-  });
+export { firebaseCloudMessaging };
 
-export { firebaseConfig, analytics, firestore, getMessagingToken, onMessageListener };
+// if (firebaseConfig?.projectId && firebase.apps.length === 0) {
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+
+//   if (app.name && typeof window !== 'undefined') {
+//     analytics = getAnalytics(app);
+//     // eslint-disable-next-line unicorn/prefer-top-level-await
+//     isSupported().then((value) => {
+//       if (value) {
+//         messaging = getMessaging(app);
+//       }
+//     });
+//     if (!isIOS) {
+//       // eslint-disable-next-line unicorn/prefer-top-level-await
+//       Notification.requestPermission().then((value) => {
+//         if (value && value === 'granted') {
+//           allowNotificationTracking('Allow');
+//         } else {
+//           allowNotificationTracking('Not Allow');
+//         }
+//       });
+//     }
+//   }
+// } else {
+//   firebase.app();
+// }
+
+// const getMessagingToken = async () => {
+//   let currentToken = '';
+//   if (!messaging) {
+//     return;
+//   }
+//   try {
+//     currentToken = await getToken(messaging, {
+//       vapidKey: ENV.FIREBASE_VAPIDKEY,
+//     });
+//   } catch (error) {
+//     console.log('xxx An error occurred while retrieving token.', error);
+//   }
+//   return currentToken;
+// };
+
+// const onMessageListener = () =>
+//   new Promise((resolve) => {
+//     onMessage(messaging, (payload) => {
+//       resolve(payload);
+//     });
+//   });
+
+// export { firebaseConfig, analytics, firestore, getMessagingToken, onMessageListener };
