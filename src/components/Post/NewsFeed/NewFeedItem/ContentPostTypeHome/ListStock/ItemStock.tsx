8413fd7799ab830062148ff11cd2ab5c47dc3624
HoangTV <hoang.tv@agiletechsoftware.com>
2023-10-17 16:26:30 +0700

    Fix image size

--- src/components/Post/NewsFeed/NewFeedItem/ContentPostTypeHome/ListStock/ItemStock.tsx
+++ src/components/Post/NewsFeed/NewFeedItem/ContentPostTypeHome/ListStock/ItemStock.tsx
@@ -20,9 +20,9 @@ const ItemStock = ({
       className='mr-[6px] flex h-[32px] flex-row items-center justify-between rounded-[1000px] border-[1px] border-solid border-[rgba(88,157,192,0.24)] bg-[#1F6EAC] pl-[4px] pr-[6px] backdrop-blur-[2px] backdrop-filter'
     >
       <Image
-        width='0'
-        height='0'
-        sizes='100vw'
+        width='1'
+        height='1'
+        sizes='24px'
         src={imageStock(data)}
         alt=''
         className=' mr-[6px]  h-[24px] min-w-[24px] max-w-[24px]  overflow-hidden rounded-full border-2 border-solid border-white bg-[#ffffff] object-contain shadow-[0_2px_4px_rgba(0,0,0,0.10)]'
