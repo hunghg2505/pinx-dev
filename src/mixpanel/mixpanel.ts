b35f592adff20eb5a527f10e20d61e3bf25f39a1
hoangnv <hoang.nv@agiletechsoftware.com>
2023-10-19 13:34:53 +0700

    fix code

--- src/mixpanel/mixpanel.ts
+++ src/mixpanel/mixpanel.ts
@@ -67,7 +67,11 @@ export const getMoreInfoTracking = (screenName: string, infoGroup: string, infoD
   });
 };
 
-export const registerTracking = (startRegistration: Date, CTALocation: string, CTAType: string) => {
+export const registerTracking = (
+  startRegistration: string,
+  CTALocation: string,
+  CTAType: string,
+) => {
   mixpanel.register({
     'KYC Status': 'Start Register Account',
   });
