c257052b128dc871222aca2e65feacc2b23edc69
hunghg255 <giahunghust@gmail.com>
2023-11-02 12:18:02 +0700

    chore: revert partytown

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { Partytown } from '@builder.io/partytown/react';
 import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
@@ -7,9 +8,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <Partytown forward={['dataLayer.push']} />
         <Script
           id='gtag'
-          strategy='lazyOnload'
+          strategy='afterInteractive'
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -17,6 +19,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${ENV.GOOGLE_TAG_MANAGER_ID}');`,
           }}
+          type='text/partytown'
         ></Script>
       </Head>
       <body>
